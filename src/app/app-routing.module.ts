6ba7c5184aa39be4039698aa626100c2794f066a
MSILycanthropy <ethanmichaelk@gmail.com>
2020-12-29 17:59:27 -0600

    adding new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
 import { GeneratedListComponent } from './components/generated-list/generated-list.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { TermsOfUseComponent } from './components/terms-of-use/terms-of-use.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: LandingComponent },
   { path: 'list', component: GeneratedListComponent },
+  { path: 'about/terms', component: TermsOfUseComponent },
+  { path: 'about', component: AboutComponent },
 ];
 
 @NgModule({
