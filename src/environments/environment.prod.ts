a809e5355237b868e4811437325432e864495879
MSILycanthropy <ethanmichaelk@gmail.com>
2020-12-31 20:19:16 -0600

    shitty gamer protection

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   siteUrl: 'https://rngmanga.com',
   apiUrl: 'https://test.rngmanga.com',
+  SECRET: '47eecfdece6dfb851fec9b2b7bdfe48c71cb0008f2728d32250a578f172b849c',
 };
