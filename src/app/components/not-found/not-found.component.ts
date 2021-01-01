440afe8c79cad6d45f03db08a97ae83b441ad42c
MSILycanthropy <ethanmichaelk@gmail.com>
2020-12-31 22:52:14 -0600

    Bugfixes

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+  styleUrls: ['./not-found.component.scss'],
 })
 export class NotFoundComponent implements OnInit {
+  @Input() num: number;
+  @Input() error: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
