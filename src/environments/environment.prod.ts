69c841af836082d68d6a8c9c6ebe133b302b8020
Ethan Kircher <ethanmichaelk@gmail.com>
2021-01-18 16:37:53 -0600

    test.rng -> api.rng

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   siteUrl: 'https://rngmanga.com',
-  apiUrl: 'https://test.rngmanga.com',
+  apiUrl: 'https://api.rngmanga.com',
   SECRET: '47eecfdece6dfb851fec9b2b7bdfe48c71cb0008f2728d32250a578f172b849c',
 };
