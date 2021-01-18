37fc08e7330ff1a548c3bea3aed273a25d7bd629
Ethan Kircher <ethanmichaelk@gmail.com>
2021-01-17 18:26:48 -0600

    Adding a little side nav dropdown

--- src/app/components/main-header/main-header.component.ts
+++ src/app/components/main-header/main-header.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'main-header',
   templateUrl: './main-header.component.html',
-  styleUrls: ['./main-header.component.scss']
+  styleUrls: ['./main-header.component.scss'],
 })
 export class MainHeaderComponent implements OnInit {
+  faBars = faBars;
+  showSideNav: boolean = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  toggleSideNav(): void {
+    this.showSideNav = !this.showSideNav;
+    if (this.showSideNav) {
+      document.getElementById('sideNav').style.height = '200px';
+      document.getElementById('sideNav').style.borderTop = '1px solid white';
+    } else if (!this.showSideNav) {
+      document.getElementById('sideNav').style.height = '0';
+      document.getElementById('sideNav').style.borderTop = 'none';
+    }
   }
-
 }
